149a422459864dcb066475407cb455788b687263
“chong.liu” <wb.chong.liu@renren-inc.com>
2019-09-06 14:58:43 +0800

    init

---
+++ precache-manifest.79143c922ae1da077cf33e84234e5e70.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c89eb12e73002b3a359846d8c460e02",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e7c6cc3852474e11476e",
+    "url": "/static/css/2.3334b4f5.chunk.css"
+  },
+  {
+    "revision": "94d67c3681bac1249478",
+    "url": "/static/css/main.765b2f4d.chunk.css"
+  },
+  {
+    "revision": "e7c6cc3852474e11476e",
+    "url": "/static/js/2.4c186541.chunk.js"
+  },
+  {
+    "revision": "94d67c3681bac1249478",
+    "url": "/static/js/main.3dc0c599.chunk.js"
+  },
+  {
+    "revision": "8cff6654acbc05d7b9a5",
+    "url": "/static/js/runtime~main.5a528767.js"
+  }
+]);
